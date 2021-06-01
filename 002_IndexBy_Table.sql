04a01c975bc6e1b88db50d70034fa69cfe9385d4
Fer Palma <gokuso03@gmail.com>
2021-06-01 15:28:52 -0600

    Agregados mas ejemplos con arrays asociativos (index by tables)

--- 002_IndexBy_Table.sql
+++ 002_IndexBy_Table.sql
@@ -135,3 +135,141 @@ BEGIN
 
     dbms_output.put_line('Luego de eliminar se tienen un total de empleados de : ' || lista.count);
 END;
+/
+
+/*
+
+    Ejemplo 4: Se cargan dos index_by table con datos de una tabla, el index by de departamentos solo con el nombre del departamento
+    y la de empleados con las filas enteras.
+
+*/
+SET SERVEROUTPUT ON;
+
+DECLARE
+    --Declaracion de index by 
+    TYPE ibt_departamentos IS
+        TABLE OF departments.department_name%TYPE INDEX BY PLS_INTEGER;
+    depts  ibt_departamentos;
+    TYPE ibt_empleados IS
+        TABLE OF employees%rowtype INDEX BY PLS_INTEGER;
+    emple  ibt_empleados;
+    
+    --declaracion de cursores
+    CURSOR cur_depart IS
+    SELECT
+        *
+    FROM
+        departments;
+
+    CURSOR cur_emple IS
+    SELECT
+        *
+    FROM
+        employees
+    WHERE
+        salary > 5000;
+        
+    --variables para recorrer los index by tables
+    i      PLS_INTEGER := 1;
+    z      PLS_INTEGER := 1;
+BEGIN
+    --Recorrer los cursores e introducir la data en los index by table
+    FOR dep IN cur_depart LOOP
+        depts(i) := dep.department_name;
+        i := i + 1;
+    END LOOP;
+
+    FOR empleado IN cur_emple LOOP
+        emple(z) := empleado;
+        z := z + 1;
+    END LOOP;
+    
+    --impresiones
+    FOR j IN 1..i - 1 LOOP
+        dbms_output.put_line(depts(j));
+    END LOOP;
+    
+    FOR j IN 1..z-1 LOOP
+        dbms_output.put_line(emple(j).first_name ||' '|| emple(j).salary);
+    END LOOP;
+
+END;
+/
+
+/*
+
+    Ejemplo 5: BULK COLLECT con INDEX_BY TABLE, cuando se hacen cargas masivas de datos de una tabla a un array, record etc, es mejor
+    utilizar bulk collect ya que asi se tiene un rendimiento mas optimo y es mucho mas rapido
+
+*/
+SET SERVEROUTPUT ON;
+
+DECLARE
+    --Declaracion de index by 
+    TYPE ibt_empleados IS
+        TABLE OF employees%rowtype INDEX BY PLS_INTEGER;
+    emple ibt_empleados;
+BEGIN
+
+    --En este caso el bulk collect se usa en un select into para cargar todos los datos de la query (donde se usaba el CURSOR)
+    SELECT
+        *
+    BULK COLLECT
+    INTO emple
+    FROM
+        employees
+    WHERE
+        salary > 5000;
+        
+    --El primer numero que pone el bulk collect es 1 y va de uno en uno hasta el final.
+    FOR i in 1..emple.last LOOP
+        dbms_output.put_line(emple(i).first_name ||' '|| emple(i).salary);
+    END LOOP;
+
+END;
+/
+
+/*
+    Ejemplo 6: Uso de metodos principales de los index by tables.
+*/
+
+DECLARE
+  
+   TYPE SUMA_SALARIOS IS RECORD 
+      (
+         NOMBRE DEPARTMENTS.DEPARTMENT_NAME%TYPE,
+         SUMA_SALARIOS NUMBER
+      );
+
+  TYPE SUMA_SAL IS TABLE OF SUMA_SALARIOS INDEX BY PLS_INTEGER;
+ 
+  
+  SALARIOS SUMA_SAL;
+  
+BEGIN
+     SELECT DEPARTMENT_NAME,SUM(SALARY)
+     BULK COLLECT INTO SALARIOS
+     FROM EMPLOYEES JOIN DEPARTMENTS USING (DEPARTMENT_ID)
+     GROUP BY DEPARTMENT_NAME;
+    
+     FOR I IN 1..SALARIOS.COUNT() LOOP
+        DBMS_OUTPUT.PUT_LINE(SALARIOS(I).NOMBRE||' '||SALARIOS(I).SUMA_SALARIOS);
+     END LOOP;
+     
+     DBMS_OUTPUT.PUT_LINE('NÚMERO EMPLEADOS:'||SALARIOS.COUNT());
+     DBMS_OUTPUT.PUT_LINE('PRIMER REGISTRO:'||SALARIOS.FIRST());
+     DBMS_OUTPUT.PUT_LINE('ULTIMO REGISTRO:'||SALARIOS.LAST());
+     IF SALARIOS.EXISTS(20) THEN
+        DBMS_OUTPUT.PUT_LINE('EXISTE');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('NO EXISTE');
+    END IF;
+    SALARIOS.DELETE(1);
+    DBMS_OUTPUT.PUT_LINE('PRIMER REGISTRO:'||SALARIOS.FIRST());
+    DBMS_OUTPUT.PUT_LINE(SALARIOS.PRIOR(3));
+  DBMS_OUTPUT.PUT_LINE(SALARIOS.NEXT(3));
+    SALARIOS.DELETE(4);
+  DBMS_OUTPUT.PUT_LINE(SALARIOS.PRIOR(3));
+  DBMS_OUTPUT.PUT_LINE(SALARIOS.NEXT(3));
+END;
+/
